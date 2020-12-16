b33e652c3dae8a7f7a9159915e06eca250106329
Zsofia Szego <szegozsofia@gmail.com>
2020-12-16 11:38:27 +0100

    add  basic sql methods

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoJdbc.java
@@ -6,25 +6,59 @@ import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
 
+import javax.sql.DataSource;
+import java.sql.*;
 import java.util.List;
 
 public class ProductDaoJdbc implements ProductDao {
 
+    private DataSource dataSource;
 
+    public ProductDaoJdbc(DataSource dataSource){this.dataSource = dataSource;}
 
     @Override
     public void add(Product product) {
-
+        try (Connection connection = dataSource.getConnection()){
+            String sql = "INSERT INTO products (name, description, default_price, currency, supplier, product_category) " +
+                    "VALUES (?, ?, ?, ?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, product.getName());
+            statement.setString(2, product.getDescription());
+            statement.setFloat(3, product.getDefaultPrice());
+            statement.setString(4, product.getDefaultCurrency().toString());
+            statement.setInt(5, product.getSupplier().getId());
+            statement.setInt(6, product.getProductCategory().getId());
+            statement.executeUpdate();
+            ResultSet resultSet = statement.getGeneratedKeys();
+            resultSet.next();
+            product.setId(resultSet.getInt(1));
+        } catch (SQLException e){throw new RuntimeException(e);}
     }
 
     @Override
     public Product find(int id) {
-        return null;
+        try (Connection connection = dataSource.getConnection()){
+            String sql = "SELECT id, name, description, default_price, currency, supplier, product_category FROM products WHERE id = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (! resultSet.next() ){return null;}
+            ProductCategoryDaoJdbc productCategoryDaoJdbc = new ProductCategoryDaoJdbc(dataSource);
+            SupplierDaoJdbc supplierDaoJdbc = new SupplierDaoJdbc(dataSource);
+            Product product = new Product(resultSet.getString(2), resultSet.getFloat(4), resultSet.getString(5), resultSet.getString(3),
+                    productCategoryDaoJdbc.find(resultSet.getInt(6)), supplierDaoJdbc.find(resultSet.getInt(7)));
+            product.setId(resultSet.getInt(1));
+            return product;
+        } catch (SQLException e){throw new RuntimeException(e);}
     }
 
     @Override
     public void remove(int id) {
-
+        try (Connection connection = dataSource.getConnection()){
+            String sql ="DELETE FROM products WHERE id= ? ";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, id);
+        } catch (SQLException e){throw new RuntimeException(e);}
     }
 
     @Override
